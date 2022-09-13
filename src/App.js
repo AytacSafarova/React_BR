d2766ffcea6902aa2035d62d21dc5ba981cc6bca
Aytaj Safarova <aytaj.ps@code.local>
2022-09-13 18:40:20 +0400

    ready

--- src/App.js
+++ src/App.js
@@ -3,28 +3,26 @@ import Projects from './Projects';
 import Navbar from './Navbar';
 import Header from './Header';
 import About from './About';
-import Contact  from './Contact';
+import Contact from './Contact';
 import Map from './Map';
 import Footer from './Footer';
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Header/>
-     <Projects/>  
-     <div className="w3-content w3-padding" style={{maxWidth:'1564px'}}>
-
-   <About/>
-    <Contact/>
-    <Map/>
-    </div>
-
-    <Footer/>    
+      <Navbar />
+      <Header />
+      <div className="w3-content w3-padding" style={{ maxWidth: '1564px' }}>
+        <Projects />
+        <About />
+        <Contact />
+        <Map />
+      </div>
+      <Footer />
     </>
 
 
-   );
+  );
 }
 
 export default App;
--- src/Projects.js
+++ src/Projects.js
@@ -1,39 +1,38 @@
 import React from "react";
-import '../src/Projects.css'
 function Projects() {
-   let  data1 = [
-         {
+    let data1 = [
+        {
             title: "Summer House",
             src: "https://www.w3schools.com/w3images/house5.jpg",
         },
-      {
+        {
             title: "Brick House",
             src: "https://www.w3schools.com/w3images/house2.jpg",
         },
-       {
+        {
             title: "Renovated",
             src: "https://www.w3schools.com/w3images/house3.jpg",
         },
-     {
+        {
             title: "Barn House",
             src: "https://www.w3schools.com/w3images/house4.jpg",
         },
-   
+
     ]
-    let data2=[
+    let data2 = [
         {
             title: "Summer House",
             src: "https://www.w3schools.com/w3images/house2.jpg",
         },
-      {
+        {
             title: "Brick House",
             src: "https://www.w3schools.com/w3images/house5.jpg",
         },
-   {
+        {
             title: "Renovated",
             src: "https://www.w3schools.com/w3images/house4.jpg",
         },
-      {
+        {
             title: "Barn House",
             src: "https://www.w3schools.com/w3images/house3.jpg",
         }
@@ -41,21 +40,37 @@ function Projects() {
     ]
 
 
-
     return (
         <div>
-            <h1>Projects</h1>
-            <div className="container">
-           { data.map(element => {
-         return   <div>
-                <img src={element.src}/>
-                <h2>{element.title}</h2>
-                </div>
-            })
-        }
-                    </div>
-
+            <div className="w3-container w3-padding-32" id="projects">
+                <h3 className="w3-border-bottom w3-border-light-grey w3-padding-16">Projects</h3>
+            </div>
+            <div className="w3-row-padding">
+                {
+                    data1.map(element => {
+                        return <div className="w3-col l3 m6 w3-margin-bottom">
+                            <div className="w3-display-container">
+                                <div className="w3-display-topleft w3-black w3-padding">{element.title}</div>
+                                <img src={element.src} alt="House" style={{ width: '100%' }} />
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
+            <div className="w3-row-padding">
+                {
+                    data2.map(element => {
+                        return <div className="w3-col l3 m6 w3-margin-bottom">
+                            <div className="w3-display-container">
+                                <div className="w3-display-topleft w3-black w3-padding">{element.title}</div>
+                                <img src={element.src} alt="House" style={{ width: '100%' }} />
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
         </div>
+
     )
 }
 
